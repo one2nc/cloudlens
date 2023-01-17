9f70c2581384a6db1645db396b557870b46de0a4
aanamshaikh <aanamshaikh01@gmail.com>
2023-01-17 17:34:12 +0530

    refactor code

--- internal/aws/ec2.go
+++ internal/aws/ec2.go
@@ -18,20 +18,10 @@ type EC2Resp struct {
 	MonitoringState  string
 	LaunchTime       string
 }
+
 type Ec2Service interface {
 	GetInstances(sess session.Session) ([]EC2Resp, error)
-	// CreateSecGrp(name, desc, vpcId string)
-	// CreateKeyPair(keyName string) (*ec2.CreateKeyPairOutput, error)
-	// ListAllKeyPairs() ([]*string, error)
 }
-
-// type ec2Service struct {
-// 	client ec2.EC2
-// }
-
-// func NewEc2Service(sess session.Session) Ec2Service {
-// 	return ec2Service{client: *ec2.New(&sess)}
-// }
 
 func GetAllRegions() []string {
 	return []string{"us-east-1", "us-east-2", "us-west-1", "us-west-2", "af-south-1", "ap-east-1", "ap-south-2", "ap-southeast-3", "ap-south-1", "ap-northeast-3", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "ca-central-1", "eu-central-1", "eu-west-1", "eu-west-2", "eu-south-1", "eu-west-3", "eu-south-2", "eu-north-1", "eu-central-2", "me-south-1", "me-central-1", "sa-east-1", "us-gov-east-1", "us-gov-west-1"}
@@ -45,7 +35,6 @@ func GetInstances(sess session.Session) ([]EC2Resp, error) {
 		fmt.Println("Error fetching instances:", err)
 		return nil, err
 	}
-
 	// Iterate through the instances and print their ID and state
 	for _, reservation := range result.Reservations {
 		for _, instance := range reservation.Instances {
@@ -66,61 +55,3 @@ func GetInstances(sess session.Session) ([]EC2Resp, error) {
 	}
 	return ec2Info, nil
 }
-
-// func CreateSecGrp(name, desc, vpcId string) {
-// 	if name == "" || desc == "" {
-// 		log.Fatal("Group name and description require")
-// 	}
-
-// 	if vpcId == "" {
-// 		vpc, err := e.client.DescribeVpcs(nil)
-// 		if err != nil {
-// 			log.Fatalf("Unable to describe VPCs, %v", err)
-// 		}
-// 		if len(vpc.Vpcs) == 0 {
-// 			log.Fatal("No VPCs found to associate security group with.")
-// 		}
-// 		vpcId = aws.StringValue(vpc.Vpcs[0].VpcId)
-// 	}
-// 	createRes, err := e.client.CreateSecurityGroup(&ec2.CreateSecurityGroupInput{
-// 		GroupName:   aws.String(name),
-// 		Description: aws.String(desc),
-// 		VpcId:       aws.String(vpcId),
-// 	})
-// 	if err != nil {
-// 		if aerr, ok := err.(awserr.Error); ok {
-// 			switch aerr.Code() {
-// 			case "InvalidVpcID.NotFound":
-// 				log.Fatalf("invalid VPCID")
-// 			case "InvalidGroup.Duplicate":
-// 				log.Fatalf("Security group already exists.")
-// 			}
-// 		}
-// 		log.Fatalf("Unable to create security group %v", err)
-// 	}
-
-// 	fmt.Printf("Created security group %s with VPC %s.\n",
-// 		aws.StringValue(createRes.GroupId), vpcId)
-// }
-
-// func CreateKeyPair(keyName string) (*ec2.CreateKeyPairOutput, error) {
-// 	result, err := e.client.CreateKeyPair(&ec2.CreateKeyPairInput{
-// 		KeyName: aws.String(keyName),
-// 	})
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	return result, nil
-// }
-
-// func ListAllKeyPairs() ([]*string, error) {
-// 	allKeyPairs, err := e.client.DescribeKeyPairs(nil)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	var keyPairsNames []*string
-// 	for _, pair := range allKeyPairs.KeyPairs {
-// 		keyPairsNames = append(keyPairsNames, pair.KeyName)
-// 	}
-// 	return keyPairsNames, err
-// }
