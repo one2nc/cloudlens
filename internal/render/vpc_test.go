a1b711520388b93fa5ebd429dd94295e54839207
chinmaysomani <chinmaysomani7@gmail.com>
2023-04-03 17:51:26 +0530

    add UI test for VPC

---
+++ internal/render/vpc_test.go
@@ -0,0 +1,21 @@
+package render
+
+import (
+	"testing"
+
+	"github.com/one2nc/cloudlens/internal/aws"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVpcRender(t *testing.T) {
+	pom := aws.VpcResp{VpcId: "vpc-1", OwnerId: "000000000000", CidrBlock: "172.31.0.0/16", InstanceTenancy: "default", State: "available"}
+
+	var vpc VPC
+	r := NewRow(5)
+	err := vpc.Render(pom, "vpc", &r)
+	assert.Nil(t, err)
+
+	assert.Equal(t, "vpc", r.ID)
+	e := Fields{"vpc-1", "000000000000", "172.31.0.0/16", "default", "available"}
+	assert.Equal(t, e, r.Fields[0:])
+}
